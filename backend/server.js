7836dcef8bbedcebc9ed3dbb99f460b23c5c5866
chamarakq05 <chamarakq05@gmail.com>
2026-06-26 09:27:49 +0530

    Update server response and logging format

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,15 @@ const app = express();
 
 app.use(express.json());
 
+
 app.get("/", (req,res)=>{
-    res.json({
-        status:"Mega SicBo System Running"
-    });
+    res.send("Mega SicBo Backend Running");
 });
 
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,"0.0.0.0",()=>{
-    console.log("Server running on port "+PORT);
+
+app.listen(PORT, "0.0.0.0", ()=>{
+    console.log(`Server running on ${PORT}`);
 });
