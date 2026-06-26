0a08b48bcccffc73af8872cd24792d03af12fee5
chamarakq05 <chamarakq05@gmail.com>
2026-06-26 09:44:50 +0530

    Fix server port and improve response logging

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,12 @@ const app = express();
 
 app.use(express.json());
 
-
 app.get("/", (req,res)=>{
-    res.send("Mega SicBo Backend Running");
+  res.send("Mega SicBo Backend Running");
 });
 
-
-const PORT = process.env.PORT || 5000;
-
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0", ()=>{
-    console.log(`Server running on ${PORT}`);
+ console.log("Server running on " + PORT);
 });
