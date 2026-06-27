f50824449b535fd2a087ee850cfed83d6bf06efd
chamarakq05 <chamarakq05@gmail.com>
2026-06-27 14:38:45 +0530

    Fix syntax error in main.jsx render method

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+
 ReactDOM.createRoot(
   document.getElementById("root")
-).render(
+)
+.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
