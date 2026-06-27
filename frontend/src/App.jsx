5d7fb96d2194ed2c531a771c933a8b75ff2a465b
chamarakq05 <chamarakq05@gmail.com>
2026-06-27 14:29:12 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,35 @@
-import React from "react";
-import Game from "./pages/Game";
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div>
-      <Game />
-    </div>
-  );
+function App(){
+
+const [status,setStatus] = useState("Loading...");
+
+
+useEffect(()=>{
+
+fetch("https://bo2-production-f1ff.up.railway.app/")
+.then(res=>res.text())
+.then(data=>{
+ setStatus(data);
+})
+.catch(err=>{
+ setStatus("Backend Error");
+});
+
+
+},[]);
+
+
+return(
+<div>
+
+<h1>Mega SicBo Dashboard</h1>
+
+<h3>{status}</h3>
+
+</div>
+)
+
 }
 
 export default App;
